7eac502a6be3fe267c154c9b8f284b0c96e1eedd
Mahaela <mahaelajohnson@yahoo.com>
2018-11-12 19:55:14 -0800

    Pokemon Search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokemon-search';
+
+	allPokemon = [];
+	displayPokemon = [];
+	showNoPokemonMessage = false;
+
+	constructor(public http: HttpClient) {
+
+		// get the list of pokemon
+		this.http.get('https://pokeapi.co/api/v2/pokemon/')
+			.subscribe((result: { results: object[] }) => this.allPokemon = result.results);
+	}
+
+	/**
+	 * search for pokemon data that have names that begin with the text in the search bar when the enter key is pressed 
+	 */
+	onEnterUp(prefix) {
+
+		// do not show any pokemon if the search bar is empty 
+		if (!prefix) {
+			this.displayPokemon = [];
+			return;
+		}
+		this.displayPokemon = _.filter(this.allPokemon, pokemon => _.startsWith(pokemon.name, prefix.toLowerCase()));
+
+		// show an error message if no pokemon are found
+		this.showNoPokemonMessage = !this.displayPokemon.length;
+		this.getPokemonData();
+	}
+
+	/**
+	 * request more data from the PokeAPI for displaying in the UI
+	 * cache the pokemon data because PokeApi limits requests to their api to 100 requests per minute
+	 */
+	getPokemonData() {
+		this.displayPokemon.forEach(pokemon => {
+
+			// there is no reason to request the pokemon's data from PokeApi if the data has already been cached
+			if (pokemon.id) return;
+			this.http.get(pokemon.url)
+				.subscribe((result: object) => {
+
+					// cache the pokemon data so a request for the same pokemon data will not need to be made again
+					_.merge(pokemon, result);
+					_.merge(this.allPokemon, result)
+				})
+		})
+	}
+
+	/**
+	 * not all pokemon have sprites, so I only show sprites for the pokemon that have one
+	 */
+	getSprite(pokemon) {
+		if (!pokemon.sprites) return null;
+		if (pokemon.sprites.front_default) return pokemon.sprites.front_default;
+		if (pokemon.sprites.back_default) return pokemon.sprites.back_default;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatCardModule, MatInputModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		MatCardModule,
+		MatInputModule,
+		MatButtonModule,
+		FlexLayoutModule,
+		MatIconModule,
+		BrowserAnimationsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
